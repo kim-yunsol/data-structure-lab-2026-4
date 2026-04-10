14c93dfdab541ce1eb2b1b6fea5d459bf7f7c85c
yunsol <kysda527@gmail.com>
2026-04-10 16:29:28 +0900

    초기 커밋.

---
+++ Car_Class/Car_Class.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car {
+protected:
+    int speed;
+
+public:
+    Car() {
+        speed = 0;
+    }
+
+    void setSpeed(int s) {
+        speed = s;
+    }
+
+    void showSpeed() {
+        cout << "Speed: " << speed << endl;
+    }
+};
+
+class SportsCar : public Car {
+public:
+    void turbo() {
+        speed += 50;
+    }
+};
+
+int main() {
+    SportsCar car;
+
+    car.setSpeed(100);
+    car.showSpeed();
+
+    car.turbo();
+    car.showSpeed();
+}
