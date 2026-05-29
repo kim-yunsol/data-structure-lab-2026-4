53448d7156d8c948cbdc2e9a3ab9d7461698adb1
kim-yunsol <kysda527@gmail.com>
2026-05-29 09:49:27 +0900

    Add Lec04 array class

---
+++ Lec04_Array_Class/Complex_Class/Complex_Class.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    Complex(double r = 0, double i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    Complex add(Complex c) {
+        return Complex(real + c.real, imag + c.imag);
+    }
+
+    void print() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex A(2.0, 3.1);
+    Complex B(5.4, 1.2);
+
+    cout << "A = ";
+    A.print();
+
+    cout << "B = ";
+    B.print();
+
+    Complex C = A.add(B);
+
+    cout << "A + B = ";
+    C.print();
+}
---
+++ Lec04_Array_Class/Polynomial_Class/Polynomial_Class.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Polynomial {
+private:
+    int coef[5]; // 최대 4차
+
+public:
+    Polynomial(int c0, int c1, int c2, int c3, int c4) {
+        coef[0] = c0;
+        coef[1] = c1;
+        coef[2] = c2;
+        coef[3] = c3;
+        coef[4] = c4;
+    }
+
+    void print() {
+        for (int i = 4; i >= 0; i--) {
+            if (coef[i] != 0) {
+                cout << coef[i] << "x^" << i;
+                if (i != 0) cout << " + ";
+            }
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Polynomial p(1, 2, 3, 0, 5); // 5x^4 + 3x^2 + 2x + 1
+    p.print();
+}
